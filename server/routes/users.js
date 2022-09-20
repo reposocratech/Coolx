22af5a2d49672be2466a4887b64e2372e9774f13
JosePosadaSanchez <sanchezposada.joseantonio@gmail.com>
2022-09-20 16:12:30 +0200

    create user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const userControllers = require('../../../../Bootcamp We Will Code You/Clases Santiago/travels/server/controllers/userControllers');
+const userController = require("../controllers/userControllers");
 var router = express.Router();
+const multer = require("../middleware/multer");
+const multerSingle = require("../middleware/multerSingle");
+const verify = require("../middleware/verify");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+
+//1 createUser
+//localhost:4000/users/createUser
+router.post("/createUser", multer("user"), userControllers.createUser);
 
 module.exports = router;
