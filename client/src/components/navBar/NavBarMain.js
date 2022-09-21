0189a0a5666d7a739c06b69d93e9e0a1f953f06d
franco <di.francoamoroso@gmail.com>
2022-09-21 17:11:17 +0200

    User v1

--- client/src/components/navBar/NavBarMain.js
+++ client/src/components/navBar/NavBarMain.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import {Navbar, Container, Nav} from "react-bootstrap"
-import "./navbar.scss"
-
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./navbar.scss";
 
 export const NavBarMain = () => {
   return (
-   <> 
-   <Navbar className='nav-color'  expand="lg">
-        <Container >
-        <Navbar.Brand className='navbar-logo' > <img src='/icons/logowhite.png'/></Navbar.Brand>
-        <Navbar.Toggle  aria-controls ="basic-navbar-nav"/>
-        <Navbar.Collapse>
-        <Nav className="me-auto">
-    
-        
-        </Nav>
-        </Navbar.Collapse>
+    <>
+      <Navbar className="nav-color" expand="lg">
+        <Container>
+          <Navbar.Brand className="navbar-logo">
+            {" "}
+            <img src="/icons/logowhite.png" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="me-auto"></Nav>
+          </Navbar.Collapse>
 
-         <button className='boton-sesion'>Iniciar sesion</button>
-           
+          <button className="boton-sesion">Iniciar sesion</button>
         </Container>
-    </Navbar>
-   
-   </>
-  )
-}
+      </Navbar>
+    </>
+  );
+};
--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -4,6 +4,11 @@ import { ContactForm } from "../components/forms/ContactForm";
 import { NavBarMain } from "../components/navBar/NavBarMain";
 import { Register } from "../pages/auth/Register";
 import { Home } from "../pages/home/Home";
+import { Messages } from "../pages/user/Messages";
+import { MyAccount } from "../pages/user/MyAccount";
+import { MyProjects } from "../pages/user/MyProjects";
+import { Reports } from "../pages/user/Reports";
+import { User } from "../pages/user/User";
 
 export const AppRoutes = () => {
   return (
@@ -14,6 +19,13 @@ export const AppRoutes = () => {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/registrocoolx" element={<Register />} />
+          <Route path="/user" element={<User />}>
+            <Route path="" element={<MyProjects />} />
+            <Route path="myprojects" element={<MyProjects />} />
+            <Route path="reports" element={<Reports />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="myaccount" element={<MyAccount />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
