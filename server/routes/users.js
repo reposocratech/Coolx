62f721ed81073cfeedf7fbef23fc5c04d7aa2ba3
barbarajg <basjgamboa@gmail.com>
2022-09-21 12:42:36 +0200

    Routes part 1

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,10 @@
 var express = require('express');
 const userControllers = require("../controllers/userControllers");
 var router = express.Router();
-const multer = require("../middleware/multer");
-const multerSingle = require("../middleware/multerSingle");
-const verify = require("../middleware/verify");
 
 
-
-//1 createUser
-//localhost:4000/users/createUser
-router.post("/createUser", userControllers.createUser);
-
-
+// 2. User login
+// localhost:4000/users/login
+router.post("/login", userControllers.login);
 
 module.exports = router;
