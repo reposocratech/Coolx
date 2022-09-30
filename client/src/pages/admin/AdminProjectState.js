92531f024bdcd9bcca4183cb893861382ab39f87
Andrea Sosa <sosa813546@gmail.com>
2022-09-30 10:01:06 +0200

    cambios andrea

--- client/src/pages/admin/AdminProjectState.js
+++ client/src/pages/admin/AdminProjectState.js
@@ -265,3 +265,376 @@ export const AdminProjectState = ({ setIsLogged }) => {
     </div>
   );
 };
+
+
+
+
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./adminProjectState.scss";
+import Table from "react-bootstrap/Table";
+import { AdminProjectModal } from "./AdminProjectModal";
+import { AdminStatusModal } from "./AdminStatusModal";
+import { AdminDeleteModal } from "./AdminDeleteModal";
+import { AdminEditModal } from "./AdminEditModal";
+import { Footer } from "../home/Footer";
+
+
+export const AdminProjectState = ({ setIsLogged }) => {
+  const [allProjects, setAllProjects] = useState();
+  const [resetProjects, setResetProjects] = useState(false);
+  const [projectModal, setProjectModal] = useState();
+  const [openModal, setOpenModal] = useState(false);
+  const [modalState, setModalState] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
+
+  const [modalEdit, setModalEdit] = useState(false);
+
+  const [tablaBusqueda, setTablaBusqueda] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [orderProjects, setOrderProjects] = useState()
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("infocoolx");
+
+    if (token) {
+      setIsLogged(true);
+
+      const { id, type } = jwtDecode(token).user;
+
+      if (type === 1) {
+        axios
+          .get(`http://localhost:4000/admin/${id}/allProjects`)
+          .then((res) => {
+            // console.log(res);
+            setAllProjects(res.data);
+            setTablaBusqueda(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        alert("No tienes permiso de administrador");
+      }
+    } else {
+      alert("Debes iniciar sección como administrador");
+    }
+  }, [resetProjects]);
+
+  // console.log(allProjects.length);
+
+  const handleModal = (project) => {
+    setProjectModal(project);
+    setOpenModal(true);
+  };
+
+  const handleStateModal = (project) => {
+    setProjectModal(project);
+    setModalState(true);
+  };
+
+  const handleDeleteModal = (project) => {
+    setProjectModal(project);
+    setModalDelete(true);
+  };
+
+  const handleEditModal = (project) => {
+    setProjectModal(project);
+    setModalEdit(true);
+  }
+
+  const handleChange = (e) => {
+    setBusqueda(e.target.value)
+    filtrar(e.target.value)
+  };
+
+  const filtrar = (terminoBusqueda) => {
+      let resBusqueda = tablaBusqueda.filter((elemento) => {
+        if(elemento.project_name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())){
+          return elemento
+        }
+      })
+      setAllProjects(resBusqueda);
+  };
+
+  const handleOrderName = () => {
+   
+   const sortedList= [...allProjects].sort((a ,b) => (a.project_name.toString().toLowerCase() > b.project_name.toString().toLowerCase() ? 1 : a.project_name.toString().toLowerCase() < b.project_name.toString().toLowerCase() ? -1 : 0))
+
+    console.log(sortedList);
+    setAllProjects(sortedList)
+   
+  }
+
+  const handleOrderId = () => {
+   
+    const sortedListId= [...allProjects].sort((a ,b) => (Number(a.project_i)> Number(b.project_id) ? 1 : Number(a.project_id) < Number(b.project_id) ? -1 : 0))
+ 
+     console.log(sortedListId);
+     setAllProjects(sortedListId)
+    
+   }
+
+   const handleOrderLocal = () => {
+
+    const sortedListLocal = [...allProjects].sort((a ,b) => (a.location.toString().toLowerCase() > b.location.toString().toLowerCase() ? 1 : a.location.toString().toLowerCase() < b.location.toString().toLowerCase() ? -1 : 0))
+
+    setAllProjects(sortedListLocal)
+
+   }
+
+   const handleOrderStatus = () => {
+
+    const sortedListStatus = [...allProjects].sort((a ,b) => (a.status > b.status ? "registrado" : a.status < b.status ? -1 : 0))
+
+    setAllProjects(sortedListStatus)
+    console.log(sortedListStatus);
+   }
+
+  return (
+<<<<<<< andrea
+    <div className="wrapper">
+      <div className="getdown">
+        <Container fluid>
+          <Row>
+            <Col className="adm-proj-state-header">
+              <Button onClick={() => navigate(-1)}>
+                <img src="./assets/icons/arrow_left.svg" />
+              </Button>
+              <h1>Estado de proyectos</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+               <input
+                  className="form-control inputBuscar "
+                  type='text'
+                  placeholder='Buscar empresa'
+                  value={busqueda}
+                  onChange={handleChange}
+                  
+                  />
+                  <Button className="btn-btn-success" onClick={()=>navigate(-1)}>
+                  Volver
+                  </Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Table striped >
+              <thead className="table-projects">
+                <tr>
+                  <th>Id <button onClick={handleOrderId}><img src="/assets/icons/arrow_donw.svg"/></button></th>
+                  <th>Nombre <button onClick={handleOrderName}><img src="/assets/icons/arrow_donw.svg"/></button></th>
+                  <th>Localización <button onClick={handleOrderLocal}><img src="/assets/icons/arrow_donw.svg"/></button></th>
+                  <th>Estado <button onClick={handleOrderStatus}><img src="/assets/icons/arrow_donw.svg"/></button></th>
+                  <th>Borrar</th>
+                  <th>Más información</th>
+                  <th>Editar</th>
+                </tr>
+              </thead>
+
+              <tbody className="list-text">
+                {allProjects &&
+                  allProjects.map((project, index) => (
+                    <tr key={project.project_id}>
+                      <td>{project.project_id}</td>
+                      <td>{project.project_name}</td>
+                      <td>{project.location}</td>
+                      <td>
+                        <div className="status-col">
+                          <p>
+                            {project.status === 0
+                              ? "Registrado"
+                              : project.status === 1
+                              ? "Calculando"
+                              : "Completado"}
+                          </p>
+=======
+    <>
+      <div className="wrapper">
+        <div className="getdown">
+          <Container fluid>
+            <Row>
+              <Col className="adm-proj-state-header">
+                <Button onClick={() => navigate(-1)}>
+                  <img src="./assets/icons/arrow_left.svg" />
+                </Button>
+                <h1>Estado de proyectos</h1>
+              </Col>
+            </Row>
+
+            <Row>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Lugar</th>
+                    <th>Estado</th>
+                    <th>Borrar</th>
+                    <th>Más información</th>
+                  </tr>
+                </thead>
+
+                <tbody className="list-text">
+                  {allProjects &&
+                    allProjects.map((project, index) => (
+                      <tr key={project.project_id}>
+                        <td>{index + 1}</td>
+                        <td>{project.project_id}</td>
+                        <td>{project.project_name}</td>
+                        <td>{project.location}</td>
+                        <td>
+                          <div className="status-col">
+                            <p>
+                              {project.status === 0
+                                ? "Registrado"
+                                : project.status === 1
+                                ? "Calculando"
+                                : "Completado"}
+                            </p>
+                            <Button
+                              type="button"
+                              className="pen-status"
+                              onClick={() => handleStateModal(project)}
+                            >
+                              <img
+                                src="/assets/icons/pen.svg"
+                                alt="Edit project state"
+                              />
+                            </Button>
+                          </div>
+                        </td>
+                        <td>
+>>>>>>> master
+                          <Button
+                            type="button"
+                            onClick={() => handleDeleteModal(project)}
+                          >
+                            Eliminar
+                          </Button>
+<<<<<<< andrea
+                        </div>
+                      </td>
+                      <td>
+                        <Button
+                          type="button"
+                          onClick={() => handleDeleteModal(project)}
+                        >
+                          Eliminar
+                        </Button>
+                      </td>
+                      <td>
+                        <Button
+                          type="button"
+                          onClick={() => handleModal(project)}
+                        >
+                          Más info
+                        </Button>
+                      </td>
+
+                      <td>
+                        <Button
+                          type="button"
+                          onClick={() => handleEditModal(project)}
+                        >
+                          Editar
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+              
+            </Table>
+          </Row>
+        </Container>
+
+        <AdminProjectModal
+          onHide={() => setOpenModal(false)}
+          show={openModal}
+          projectModal={projectModal}
+        />
+
+        <AdminStatusModal
+          onHide={() => setModalState(false)}
+          show={modalState}
+          projectModal={projectModal}
+          setModalState={setModalState}
+          setResetProjects={setResetProjects}
+          resetProjects={resetProjects}
+        />
+
+        <AdminDeleteModal
+          onHide={() => setModalDelete(false)}
+          setModalDelete={setModalDelete}
+          show={modalDelete}
+          projectModal={projectModal}
+          setResetProjects={setResetProjects}
+          resetProjects={resetProjects}
+        />
+
+        <AdminEditModal 
+          onHide={() => setModalEdit(false)}
+          setModalEdit={setModalEdit}
+          show={modalEdit}
+          projectModal={projectModal}
+          setResetProjects={setResetProjects}
+          resetProjects={resetProjects}
+          setProjectModal={setProjectModal}
+        
+        />
+
+=======
+                        </td>
+                        <td>
+                          <Button
+                            type="button"
+                            onClick={() => handleModal(project)}
+                          >
+                            Más info
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </Table>
+            </Row>
+          </Container>
+
+          <AdminProjectModal
+            onHide={() => setOpenModal(false)}
+            show={openModal}
+            projectModal={projectModal}
+          />
+
+          <AdminStatusModal
+            onHide={() => setModalState(false)}
+            show={modalState}
+            projectModal={projectModal}
+            setModalState={setModalState}
+            setResetProjects={setResetProjects}
+            resetProjects={resetProjects}
+          />
+
+          <AdminDeleteModal
+            onHide={() => setModalDelete(false)}
+            setModalDelete={setModalDelete}
+            show={modalDelete}
+            projectModal={projectModal}
+            setResetProjects={setResetProjects}
+            resetProjects={resetProjects}
+          />
+        </div>
+>>>>>>> master
+      </div>
+      <Footer />
+    </>
+  );
+};
